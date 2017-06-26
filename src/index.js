3108a5a3e2bed60a353e0ec337815c8db425f2b6
savalazic <savalazic9@gmail.com>
2017-06-26 17:32:05 +0200

    chore: starting to search

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyD1Hn5C-AiW6Oo93N8gMf2o8ln_4bDRu4o';
 
 import SearchBar from './components/SearchBar'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'arapi' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 export default App;
 
