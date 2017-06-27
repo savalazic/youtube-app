a83e776d5d600a7b74c6034e65e43cac6f95b6a8
savalazic <savalazic9@gmail.com>
2017-06-27 13:31:48 +0200

    chore: implement selected video

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyD1Hn5C-AiW6Oo93N8gMf2o8ln_4bDRu4o';
 
 import SearchBar from './components/SearchBar'
-
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'arapi' }, (videos) => {
-      this.setState({ videos });
+    YTSearch({ key: API_KEY, term: 'arapske pare' }, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -22,11 +29,13 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })} />
       </div>
     );
   }
 }
-
-export default App;
 
 ReactDOM.render(<App />, document.querySelector('.container'));
